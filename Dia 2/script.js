6be687ffbf97bbcb39646235c587f28ad7e3aa99
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2024-12-02 00:51:50 +0100

    feat: day 2

---
+++ Dia 2/script.js
@@ -0,0 +1,8 @@
+function createFrame(names) {
+    if (!names.length) return "";
+    const maxLength = Math.max(...names.map(name => name.length));
+    const border = "*".repeat(maxLength + 4);
+    const framedNames = names.map(name => `* ${name.padEnd(maxLength)} *`);
+    return [border, ...framedNames, border].join("\n");
+}
+  
