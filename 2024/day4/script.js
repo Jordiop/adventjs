5d7751a5d8e6016d02550e720b219324170a05b8
Jordi Osarenkhoe Petro <jordiosarenkhoepetro@Mac-mini-de-JordiO.local>
2024-12-04 14:53:27 +0100

    feat: day 4

---
+++ 2024/day4/script.js
@@ -0,0 +1,11 @@
+function createXmasTree(height, ornament) {
+    if (height < 1) return '';
+    let tree = '';
+    const trunk = '_'.repeat(height - 1) + '#' + '_'.repeat(height - 1) + '\n';
+    for (let i = 0; i < height; i++) {
+        const spaces = '_'.repeat(height - i - 1);
+        const decorations = ornament.repeat(2 * i + 1);
+        tree += spaces + decorations + spaces + '\n';
+    }
+    return tree + trunk + trunk.trim();
+}
