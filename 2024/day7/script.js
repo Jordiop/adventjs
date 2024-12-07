4bfc71e25a82cf800e577f4cfd96ff158fe0e17c
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2024-12-07 13:49:00 +0100

    feat: day 7

---
+++ 2024/day7/script.js
@@ -0,0 +1,9 @@
+function fixPackages(packages) {
+    while (packages.includes('(')) {
+        let start = packages.lastIndexOf('(');
+        let end = packages.indexOf(')', start);
+        let reversed = packages.slice(start + 1, end).split('').reverse().join('');
+        packages = packages.slice(0, start) + reversed + packages.slice(end + 1);
+    }
+    return packages;
+}
