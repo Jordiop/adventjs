0d68e604d4e7fe5a7af9901c3041848c7dbcdd27
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2024-12-16 21:55:18 +0100

    feat: day 16

---
+++ 2024/day16/script.js
@@ -0,0 +1,8 @@
+function removeSnow(s) {
+    let stack = [];
+    for (let char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) stack.pop();
+        else stack.push(char);
+    }
+    return stack.join('');
+}
