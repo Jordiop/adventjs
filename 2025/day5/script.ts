549cfc60b86ed32860606083cda61a72956a2e58
Jordi Osarenkhoe Petro <jordiop@MacBook-Pro-de-Jordi.local>
2025-12-06 15:14:17 +0100

    feat: day 5

---
+++ 2025/day5/script.ts
@@ -0,0 +1,21 @@
+type ElfDateTime =
+  `${number}*${number}*${number}@${number}|${number}|${number} NP`
+
+function timeUntilTakeOff(
+  fromTime: ElfDateTime,
+  takeOffTime: ElfDateTime
+): number {
+  const parseElfDateTime = (elfTime: ElfDateTime): number => {
+    const timeStr = elfTime.replace(' NP', '')
+    const [datePart, timePart] = timeStr.split('@')
+    const [year, month, day] = datePart.split('*').map(Number)
+    const [hours, minutes, seconds] = timePart.split('|').map(Number)
+    const date = Date.UTC(year, month - 1, day, hours, minutes, seconds)
+    return Math.floor(date / 1000)
+  }
+  
+  const fromTimestamp = parseElfDateTime(fromTime)
+  const takeOffTimestamp = parseElfDateTime(takeOffTime)
+
+  return takeOffTimestamp - fromTimestamp
+}
